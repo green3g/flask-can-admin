2fdf94d1d3e3283e0722718f8c13090ffaa1f895
roemhildtg <roemhildtg@gmail.com>
2016-08-08 17:48:45 -0500

    init

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='flaskcrud',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='roemhildtg',
+      author_email='roemhildtg@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Flask==0.11.1'],
      )
